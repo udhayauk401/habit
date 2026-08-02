b5a5bc3b69dcf1d76a135f759142f7cd73989a94
udhayauk401 <udhayauk401@gmail.com>
2026-08-02 20:36:13 +0530

    updated project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'client',
+  publicDir: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   plugins: [react()],
   server: {
     host: '0.0.0.0',
